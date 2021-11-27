2f61c987359499060d02869da6569df9460936c5
lokesh_jhanwar <lokeshjdh2019@gmail.com>
2021-11-27 18:13:34 +0530

    hashing and salting with bcrypt

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
+const bcrypt=require("bcrypt");
+
+const saltRounds=10;
 
 const app=express();
 
@@ -34,38 +37,38 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const username=req.body.username;
-  const password=md5(req.body.password);
-
-  const newUser=new User({
-    email:username,
-    password:password
-  });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }else{
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(!err){
+        res.render("secrets");
+      }else{
+        console.log(err);
+      }
+    });
   });
 });
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=md5(req.body.password);
+  const password=req.body.password;
   User.findOne({email:username},function(err,result){
     if(err){
       console.log(err);
     }else{
       if(result){
-        if(result.password===password){
-          console.log(result.password);
-          res.render("secrets");
-        }else{
-          res.send("Password does not match");
-        }
+        bcrypt.compare(password,result.password,function(err, result) {
+          if(result == true){
+            res.render("secrets");
+          }else{
+            res.send("Wrong Password");
+          }
+        });
       }else{
-        res.send("No use found with this Username");
+        res.send("No user found with this Username");
       }
     }
   });
